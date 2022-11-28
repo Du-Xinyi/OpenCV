e6b38747c0c7bfbbb3f2faf51ff67fdf660f5b32
堕落的黑光 <73220784+Du-Xinyi@users.noreply.github.com>
2022-11-28 23:45:57 +0800

    Add files via upload

---
+++ Divide "digital.png"/cmake-build-debug/CMakeFiles/Divide_digital_png.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Divide_digital_png.dir/main.cpp.o"
+  "Divide_digital_png"
+  "Divide_digital_png.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Divide_digital_png.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ README.md
@@ -0,0 +1,5 @@
+# #2022.11.23
+
+## 创建Divide "digital.png"
+
+划分opencv中的digits.png图片
